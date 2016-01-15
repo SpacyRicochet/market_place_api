2f3e8d63c9580b0e800e9f6baef8e911bcadf70c
Bruno Scheele <bruno@noodlewerk.com>
2016-01-15 21:08:16 +0100

    Fix issue where 'create' user is routed to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 MarketPlaceApi::Application.routes.draw do
-  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
@@ -11,4 +10,6 @@ MarketPlaceApi::Application.routes.draw do
       resources :products, :only => [:show, :index]
     end
   end
+  # devise definition
+  devise_for :users
 end
