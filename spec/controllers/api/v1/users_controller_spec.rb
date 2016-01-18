ca21f4d0d6c503720bc226dbd34d8c2db61d6451
Bruno Scheele <bruno@noodlewerk.com>
2016-01-18 15:24:08 +0100

    Embeds the products_ids into the user serializer and fetches the correct products from the index action endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,38 +3,22 @@ require 'spec_helper'
 describe Api::V1::UsersController do
 
   describe "GET #show" do
-  	context "shows user successfully" do
-			before(:each) do 
-				@user = FactoryGirl.create :user
-				get :show, id: @user.id
-			end
+		before(:each) do 
+			@user = FactoryGirl.create :user
+			get :show, id: @user.id
+		end
 
-			it "returns the information about a reporter on a hash" do
-				user_response = json_response[:user]
-				expect(user_response[:email]).to eql @user.email
-			end
-
-			it { should respond_with 200 }
+		it "returns the information about a reporter on a hash" do
+			user_response = json_response[:user]
+			expect(user_response[:email]).to eql @user.email
 		end
 		
-		# too advanced at the moment
-# 		context "cannot find user" do
-# 			before(:each) do
-# 				get :show, id: 1
-# 			end
-# 			
-# 			it "renders an errors json" do
-# 				user_response = json_response
-# 				expect(user_response).to have_key(:errors)
-# 			end
-# 			
-# 			it "renders the json errors on why the user could not be created" do
-# 				user_response = json_response
-# 				expect(user_response[:errors][:reason]).to include "user not found"
-# 			end
-# 			
-# 			it { should respond_with 404 }
-# 		end
+		it "has the product ids as an embedded object" do
+			user_response = json_response[:user]
+			expect(user_response[:product_ids]).to eql []
+		end
+
+		it { should respond_with 200 }
   end
   
   describe "POST #create" do
@@ -65,7 +49,6 @@ describe Api::V1::UsersController do
       
       it "renders the json errors on why the user could not be created" do
         user_response = json_response
-        print json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
       
