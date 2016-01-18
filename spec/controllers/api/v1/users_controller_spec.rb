35ee97eedb933cd48c61ff8d96eaa5f4b7f1f8bd
Bruno Scheele <bruno@noodlewerk.com>
2016-01-18 14:53:35 +0100

    Add user serializer for customizing the json output

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,7 +10,7 @@ describe Api::V1::UsersController do
 			end
 
 			it "returns the information about a reporter on a hash" do
-				user_response = json_response
+				user_response = json_response[:user]
 				expect(user_response[:email]).to eql @user.email
 			end
 
@@ -45,7 +45,7 @@ describe Api::V1::UsersController do
       end
       
       it "renders the json represntation for the user record just created" do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
       
@@ -65,6 +65,7 @@ describe Api::V1::UsersController do
       
       it "renders the json errors on why the user could not be created" do
         user_response = json_response
+        print json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
       
@@ -81,7 +82,7 @@ describe Api::V1::UsersController do
   		end
   		
   		it "renders the json representation for the updated user" do
-  			user_response = json_response
+  			user_response = json_response[:user]
   			expect(user_response[:email]).to eql "newmail@example.com"
   		end
   		
