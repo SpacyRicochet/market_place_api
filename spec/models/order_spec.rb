00fd2cf23cf2c6e0c7a2a483e0b8004f553e96cd
Bruno Scheele <bruno@noodlewerk.com>
2016-01-20 15:30:25 +0100

    Allows the order to be placed along with product quantity

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,7 +10,6 @@ describe Order do
 	it { should validate_presence_of :user_id }
 	
 	it { should belong_to :user }
-	
 	it { should have_many(:placements) }
 	it { should have_many(:products).through(:placements) }
 	
@@ -26,4 +25,18 @@ describe Order do
 			expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
 		end
 	end
+	
+	describe "#build_placements_with_product_ids_and_quantities" do
+		before(:each) do
+			product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+			product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+			
+			@product_ids_and_quantities = [ [product_1.id, 2], [product_2.id, 3] ]
+		end
+	
+		it "builds 2 placements for the order" do
+			expect{order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
+		end
+	end
+
 end
