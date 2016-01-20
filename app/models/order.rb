28c6c631d113e305c457174337dfd4b2c9140bfe
Bruno Scheele <bruno@noodlewerk.com>
2016-01-20 16:14:51 +0100

    Update the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,19 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  before_validation :set_total!
   
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
+
+  before_validation :set_total!
 
 	has_many :placements
   has_many :products, through: :placements
-  
+
   def set_total!
-  	self.total = products.map(&:price).sum
+  	self.total = 0
+  	placements.each do |placement|
+  		self.total += placement.product.price * placement.quantity
+  	end
   end
   
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
--- app/models/placement.rb
+++ app/models/placement.rb
@@ -2,7 +2,7 @@ class Placement < ActiveRecord::Base
   belongs_to :order, inverse_of: :placements
   belongs_to :product, inverse_of: :placements
   
-  def decrement_product_quantity
+  def decrement_product_quantity!
   	self.product.decrement(:quantity, quantity)
   end
 end
